f4ea9b8e7cc230ed6f0dfe70c2c8df9e73713c7b
Delgado Sebastian <sebdelgado83@gmail.com>
2023-01-15 14:57:51 -0300

    varios

--- src/main/java/com/portfolio/sde/Entity/hys.java
+++ src/main/java/com/portfolio/sde/Entity/hys.java
@@ -5,10 +5,15 @@
 package com.portfolio.sde.Entity;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 
 
 @Entity
 public class hys {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
     private String nombre;
     private int porcentaje;
