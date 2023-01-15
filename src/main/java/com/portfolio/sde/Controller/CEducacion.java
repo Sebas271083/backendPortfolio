e6b12d67f3637f786b5c8a0063e148038a9e0d78
Delgado Sebastian <sebdelgado83@gmail.com>
2023-01-15 13:21:13 -0300

    varios

--- src/main/java/com/portfolio/sde/Controller/CEducacion.java
+++ src/main/java/com/portfolio/sde/Controller/CEducacion.java
@@ -25,7 +25,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/educacion")
-@CrossOrigin(origins = "frontendportfolio-10265.firebaseapp.com/")
+@CrossOrigin(origins = "https://frontendportfolio-10265.firebaseapp.com/")
 public class CEducacion {
     @Autowired
     SEducacion sEducacion;
