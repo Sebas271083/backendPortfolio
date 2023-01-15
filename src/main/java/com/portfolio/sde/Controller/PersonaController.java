48c2c29c1ec71103c9e962b752c5ecba8304d362
Delgado Sebastian <sebdelgado83@gmail.com>
2023-01-15 13:20:07 -0300

    varios

--- src/main/java/com/portfolio/sde/Controller/PersonaController.java
+++ src/main/java/com/portfolio/sde/Controller/PersonaController.java
@@ -17,7 +17,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@CrossOrigin(origins = "https://frontendportfolio-10265.firebaseapp.com/")
+@CrossOrigin(origins = "frontendportfolio-10265.firebaseapp.com/")
 public class PersonaController {
     
     @Autowired
